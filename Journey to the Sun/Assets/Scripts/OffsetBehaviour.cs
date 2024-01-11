75e504c1ef7682f683696b7bad703b85956761fc
UnnamedEntity <rohan.sim@outlook.com>
2024-01-11 00:14:57 +0000

    Player Attack added
    Added player attacking in 4 directions

---
+++ Journey to the Sun/Assets/Scripts/OffsetBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetBehaviour : MonoBehaviour
+{
+    public PlayerMovement PlayerMovement;
+    public bool canShoot = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.position = PlayerMovement.transform.position + new Vector3(-1.5f, 0, 0);
+        //    canShoot = false;
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.position = PlayerMovement.transform.position + new Vector3(1.5f, 0, 0);
+        //    canShoot = false;
+        //}
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    transform.position = PlayerMovement.transform.position + new Vector3(0, 1.5f, 0);
+        //    canShoot = false;
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    transform.position = PlayerMovement.transform.position + new Vector3(0, -1.5f, 0);
+        //    canShoot = false;
+        //}
+    }
+}
