6340521e62aa76cda3c50001330d0ea7175e99be
UnnamedEntity <rohan.sim@outlook.com>
2024-02-14 20:13:20 +0000

    Skull projectile issues

--- Journey to the Sun/Assets/Scripts/Enemy Scripts/EnemyProjectileScripts/SkullProjectileBehaviour.cs
+++ Journey to the Sun/Assets/Scripts/Enemy Scripts/EnemyProjectileScripts/SkullProjectileBehaviour.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class SkullProjectileBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Player;
+    public GameObject SkullScriptObject;
+
+    SpriteRenderer sprite;
+    SkullScript SkullScript;
+
+    Vector3 direction;
+
+    int speed = 7;
+    private void Awake()
     {
-        
+        SkullScriptObject = GameObject.Find("SkullScriptObject");
+        SkullScript = SkullScriptObject.GetComponent<SkullScript>();
+
+        direction = SkullScript.direction;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        Player = GameObject.Find("Player");
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        sprite.sortingOrder = 1;
+        direction = (direction - sprite.transform.position).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(direction);
+        transform.position += direction * speed * Time.deltaTime;
     }
 }
--- Journey to the Sun/Assets/Scripts/Enemy Scripts/SkullScript.cs
+++ Journey to the Sun/Assets/Scripts/Enemy Scripts/SkullScript.cs
@@ -4,16 +4,66 @@ using UnityEngine;
 
 public class SkullScript : MonoBehaviour
 {
+    public GameObject Player;
+    PlayerBehaviour PlayerBehaviour;
+
+    public GameObject RoomControllerObject;
+    RoomController RoomController;
+
+    public GameObject SkullProjectile;
+
+    EnemyBehaviour EnemyBehav;
+
+    int randomTime;
+    float timeElapsed;
+
+    Vector3 targetCoord;
+    int projectileSpeed = 5;
+
+    public Vector3 direction;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.Find("Player");
+        PlayerBehaviour = Player.GetComponent<PlayerBehaviour>();
+
+        RoomControllerObject = GameObject.Find("RoomController");
+        RoomController = RoomControllerObject.GetComponent<RoomController>();
+
+        EnemyBehav = GetComponent<EnemyBehaviour>();
+
+        randomTime = Random.Range(2, 5);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (timeElapsed < randomTime)
+        {
+            timeElapsed += Time.deltaTime;
+        }
+        else
+        {
+            CreateProjectiles();
+            timeElapsed = 0;
+            randomTime = Random.Range(5, 8);
+        }
+
+    }
+
+    void CreateProjectiles()
+    {
+        if (RoomController.GetWorldCoord(PlayerBehaviour.playerRoomCoord) == EnemyBehav.enemyWorldCoord)
+        {
+            direction = Vector3.up;
+            Instantiate(SkullProjectile, transform.position, transform.rotation);
+            direction = Vector3.down;
+            Instantiate(SkullProjectile, transform.position, transform.rotation);
+            direction = Vector3.left;
+            Instantiate(SkullProjectile, transform.position, transform.rotation);
+            direction = Vector3.right;
+            Instantiate(SkullProjectile, transform.position, transform.rotation);
+        }
     }
 }
